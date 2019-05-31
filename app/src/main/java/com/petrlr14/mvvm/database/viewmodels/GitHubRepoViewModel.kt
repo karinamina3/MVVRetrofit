bfda0231c3239719af74e00594475a83aa54e61f
karinamina3 <00012217@uca.edu.sv>
2019-05-31 14:42:34 -0600

    Laboratorio 8

--- app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
+++ app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
@@ -1,27 +1,46 @@
 package com.petrlr14.mvvm.database.viewmodels
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.petrlr14.mvvm.database.RoomDB
 import com.petrlr14.mvvm.database.entities.GitHubRepo
 import com.petrlr14.mvvm.database.repositories.GitHubRepoRepository
+import kotlinx.coroutines.launch
 
 class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) {
 
     private val repository: GitHubRepoRepository
 
     init {
-        val repoDao=RoomDB.getInstance(app).repoDao()
-        repository= GitHubRepoRepository(repoDao)
+        val repoDao = RoomDB.getInstance(app).repoDao()
+        repository = GitHubRepoRepository(repoDao)
     }
 
-    private suspend fun insert(repo:GitHubRepo)=repository.insert(repo)
+    fun retrieveRepo(user: String) = viewModelScope.launch {
+        this@GitHubRepoViewModel.nuke()
+        val response = repository.retrieverRepoAsync(user).await()
+        if (response.isSuccessful) with(response) {
+            this.body()?.forEach {
+                this@GitHubRepoViewModel.insert(it)
+            }
+        } else with(response) {
+            when (this.code()) {
+                404 -> {
+                    Toast.makeText(app, "Usuario no encontrado", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
 
-    fun getAll():LiveData<List<GitHubRepo>>{
+    private suspend fun insert(repo: GitHubRepo) = repository.insert(repo)
+
+    fun getAll(): LiveData<List<GitHubRepo>> {
         return repository.getAll()
     }
 
-    private suspend fun nuke()= repository.nuke()
+    private suspend fun nuke() = repository.nuke()
 
 }
